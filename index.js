d9ecb002892bdc97c07a6d17df9f5cebce52d92b
liyinan <liyinan@baidu.com>
2017-01-27 15:33:48 +0800

    Initialize Kibana Plugin (generator-kibana-plugin v5.0.1)

---
+++ index.js
@@ -0,0 +1,36 @@
+import exampleRoute from './server/routes/example';
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+
+    uiExports: {
+      
+      app: {
+        title: 'Kibana Timeline',
+        description: 'An awesome Kibana plugin',
+        main: 'plugins/kibana_timeline/app'
+      },
+      
+      
+      hacks: [
+        'plugins/kibana_timeline/hack'
+      ]
+      
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+    
+    init(server, options) {
+      // Add server routes and initalize the plugin here
+      exampleRoute(server);
+    }
+    
+
+  });
+};
